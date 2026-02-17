6122b3f2eb484fe96005c5bd889f875c30ff41d4
BoduBhai <bodruddozaaraf@gmail.com>
2026-02-17 11:31:07 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Display page location
+document.getElementById("page-location").textContent = window.location.href;
+
+// Display last modified date
+const lastModified = document.lastModified;
+document.getElementById("last-modified").textContent = lastModified;
